c5fd0d315280942c4a7d8c023981a96308a7fdd3
HK9750 <hasnainkhan9750@gmail.com>
2026-01-25 17:56:35 +0500

    Implement ListNode and DoublyLinkedList methods

--- 02-LFU-Cache/main.go
+++ 02-LFU-Cache/main.go
@@ -1,16 +1,49 @@
 package main
 
-type ListNode struct{}
+type ListNode struct {
+	Key   int
+	Value int
+	Freq  int
+	Prev  *ListNode
+	Next  *ListNode
+}
 
-type DoublyLinkedList struct{}
+type DoublyLinkedList struct {
+	Size int
+	Head *ListNode
+	Tail *ListNode
+}
 
-func (dl *DoublyLinkedList) addNode(node *ListNode) {}
+func NewDoublyLinkedList() *DoublyLinkedList {
+	return &DoublyLinkedList{
+		Size: 0,
+		Head: &ListNode{},
+		Tail: &ListNode{},
+	}
+}
 
-func (dl *DoublyLinkedList) remove(node *ListNode) {}
+func (dl *DoublyLinkedList) addNode(node *ListNode) {
+	node.Prev = dl.Head
+	node.Next = dl.Head.Next
+	dl.Head.Next.Prev = node
+	dl.Head.Next = node
+	dl.Size++
+}
 
-func (dl *DoublyLinkedList) removeLast(node *ListNode) {}
+func (dl *DoublyLinkedList) remove(node *ListNode) {
+	node.Prev.Next = node.Next
+	node.Next.Prev = node.Prev
+	dl.Size--
+}
 
-type LFUCache struct{}
+func (dl *DoublyLinkedList) removeLast() *ListNode {
+	last := dl.Tail.Prev
+	dl.remove(last)
+	return last
+}
+
+type LFUCache struct {
+}
 
 func (lfu *LFUCache) get() {}
 
